b431f0782794c316bf5cf33fb5feda7731f0807a
Ld-monkey <lfourteau91@gmail.com>
2023-10-04 15:07:25 +0200

    Add green face animation with reload button.

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,22 +1,36 @@
-import ReactVivus from 'react-vivus';
+import { useState } from 'react';
+import ReactVivus from '../ReactVivus/ReactVivus';
 import GreenFace from './green-face.svg';
+import Icon from '../Icons/Icons';
 import './Hero.css';
 
 function Hero() {
+  const [animation, setAnimation] = useState(null);
+
+  const handleResetAnimation = () => {
+    if (animation) {
+      animation.reset().play();
+    }
+  };
+
   return (
     <div className="hero-container">
       <div className="hero-image">
         <ReactVivus
+          replay={setAnimation}
           id="green-face"
           option={{
             file: GreenFace,
-            duration: 150,
+            duration: 200,
             animTimingFunction: 'EASE',
             type: 'oneByOne',
             onReady: console.log,
           }}
           callback={console.log}
         />
+        <button type="button" onClick={handleResetAnimation} className="btn-reset">
+          <Icon icon="reload" size={26} color="white" />
+        </button>
       </div>
       <h1>
         Ludovic Fourteau <br />
