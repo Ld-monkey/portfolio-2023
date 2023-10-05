d61548910d3ad78051f6ba6f4df1b6891957dc77
Ld-monkey <lfourteau91@gmail.com>
2023-10-05 22:19:09 +0200

    Simple aside navbar.

--- src/components/Navbar/AsideMenu/AsideMenu.jsx
+++ src/components/Navbar/AsideMenu/AsideMenu.jsx
@@ -1,5 +1,37 @@
-function AsideMenu() {
-  return <div>AsideMenu</div>;
+import Icon from '../../Icons/Icons';
+import './AsideMenu.css';
+
+function AsideMenu({ toggled }) {
+  return (
+    <aside className={toggled && 'is-active'}>
+      <nav>
+        <ul>
+          <li>
+            <a href="#hero-section">Accueil</a>
+          </li>
+          <li>
+            <a href="#about-section">&Agrave; propos</a>
+          </li>
+          <li>
+            <a href="#projects-section">Réalisations</a>
+          </li>
+          <li>
+            <a href="#contact-setction">Contact</a>
+          </li>
+          <li>
+            <a href="mailto:">
+              <Icon icon="mail" size={27} color="white" />
+            </a>
+          </li>
+          <li>
+            <a href="https://github.com/Ld-monkey">
+              <Icon icon="github" size={26} color="white" />
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </aside>
+  );
 }
 
 export default AsideMenu;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,47 +2,51 @@ import { useState } from 'react';
 import Hamburger from 'hamburger-react';
 import Icon from '../Icons/Icons';
 import './Navbar.css';
+import AsideMenu from './AsideMenu/AsideMenu';
 
 function Navbar() {
   const [isOpen, setOpen] = useState(false);
 
   return (
-    <header>
-      <nav>
-        <ul>
-          <li>
-            <a href="#hero-section">Accueil</a>
-          </li>
-          <li>
-            <a href="#about-section">&Agrave; propos</a>
-          </li>
-          <li>
-            <a href="#projects-section">Réalisations</a>
-          </li>
-          <li>
-            <a href="#contact-setction">Contact</a>
-          </li>
-          <li>
-            <a href="mailto:">
-              <Icon icon="mail" size={27} color="white" />
-            </a>
-          </li>
-          <li>
-            <a href="https://github.com/Ld-monkey">
-              <Icon icon="github" size={26} color="white" />
-            </a>
-          </li>
-        </ul>
-        <div className="hamburger-container">
-          <Hamburger
-            toggled={isOpen}
-            toggle={setOpen}
-            size={24}
-            label="Show menu"
-          />
-        </div>
-      </nav>
-    </header>
+    <>
+      <header>
+        <nav>
+          <ul>
+            <li>
+              <a href="#hero-section">Accueil</a>
+            </li>
+            <li>
+              <a href="#about-section">&Agrave; propos</a>
+            </li>
+            <li>
+              <a href="#projects-section">Réalisations</a>
+            </li>
+            <li>
+              <a href="#contact-setction">Contact</a>
+            </li>
+            <li>
+              <a href="mailto:">
+                <Icon icon="mail" size={27} color="white" />
+              </a>
+            </li>
+            <li>
+              <a href="https://github.com/Ld-monkey">
+                <Icon icon="github" size={26} color="white" />
+              </a>
+            </li>
+          </ul>
+          <div className="hamburger-container">
+            <Hamburger
+              toggled={isOpen}
+              toggle={setOpen}
+              size={24}
+              label="Show menu"
+            />
+          </div>
+        </nav>
+      </header>
+      <AsideMenu toggled={isOpen} />
+    </>
   );
 }
 
