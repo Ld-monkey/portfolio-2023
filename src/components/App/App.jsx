c817015e3cd409d3e5e9ff0026a72e9076347dd4
Ld-monkey <lfourteau91@gmail.com>
2023-10-06 12:51:07 +0200

    vite import image

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,13 +2,14 @@ import Card from '../Card/Card';
 import Hero from '../Hero/Hero';
 import Navbar from '../Navbar/Navbar';
 import ScrollingButton from '../ScrollingButton/ScrollingButton';
+import profileImage from '/images/backgrounds/header-pink-blur.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <div className="top-circle">
-        <img src="/images/backgrounds/header-pink-blur.svg" alt="top circle" />
+        <img src={profileImage} alt="top circle" />
       </div>
       <section id="hero-section">
         <Navbar />
