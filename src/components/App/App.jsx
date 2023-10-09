047cf11508122a4228e2aa17a0f6ca2e653876c9
Ld-monkey <lfourteau91@gmail.com>
2023-10-09 12:55:55 +0200

    Create Card component.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -20,7 +20,50 @@ function App() {
       </section>
       <section id="about-section">
         <h3>&Agrave; Propros</h3>
-        <Card />
+        <div className="cards-wrapper">
+          <Card>
+            <p>
+              Passionné par l’informatique, j’ai terminé mes études pour devenir
+              ingénieur en bio-informatique.
+            </p>
+            <p>
+              Après 3 ans, à programmer pour les entreprises, j’ai décidé
+              d’élargir mes compétences dans le développement web en passant le
+              titre professionnel.
+            </p>
+            <p>
+              J’ai pu approfondir mes connaissances en HTML, CSS, JavaScript et
+              React avec des projets réalisés seul ou à plusieurs.
+            </p>
+            <p>
+              Par-dessus tout, j’aime apprendre de nouvelles choses et partager
+              mes connaissances.
+            </p>
+          </Card>
+          <Card>
+            <ul className="list-about-me">
+              <li className="list-icon diploma">Dipômes</li>
+              <ul>
+                <li>
+                  Titre Professionnel DWWM <span>(sept. 2023)</span>
+                </li>
+                <li>
+                  Master de Bio-Informatique <span>(avr. 2020)</span>
+                </li>
+              </ul>
+            </ul>
+            <ul className="list-about-me">
+              <li className="">Expérience</li>
+              <ul>
+                <li>5 ans d&apos;expériences dans la programmation</li>
+                <li>
+                  3 ans de travail dans les entreprises de la recherche
+                  publiques
+                </li>
+              </ul>
+            </ul>
+          </Card>
+        </div>
       </section>
     </div>
   );
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,12 +1,11 @@
-function Card() {
+import './Card.css';
+
+function Card({ children }) {
   return (
-    <div>
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam possimus at nisi! Minus error cupiditate praesentium
-      eveniet, sunt, incidunt cum, aperiam explicabo odio dolor cumque excepturi dolorum quis dicta porro! Lorem ipsum,
-      dolor sit amet consectetur adipisicing elit. Ipsa illum odit similique, quidem laboriosam ratione labore amet
-      minima pariatur eveniet veniam, quas facilis, magni omnis exercitationem fugit? Iste, eaque autem. Lorem, ipsum
-      dolor sit amet consectetur adipisicing elit. Ab ea ut consectetur aspernatur quasi sit quaerat, officia cumque
-      odio, impedit quidem maxime eius ad. Natus quibusdam explicabo itaque quod reiciendis.
+    <div className="card-container">
+      <div className="card-blur-circle top-background" />
+      <div className="card-blur-circle bottom-background" />
+      {children}
     </div>
   );
 }
