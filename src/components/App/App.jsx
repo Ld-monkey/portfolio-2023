54ee08a7639271ba98ef2bdfca4747ada4758dc3
Ld-monkey <lfourteau91@gmail.com>
2023-10-09 21:46:58 +0200

    Add background blur circles in second section.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -19,56 +19,61 @@ function App() {
         </div>
       </section>
       <section id="about-section">
-        <h3>&Agrave; Propros</h3>
-        <div className="cards-wrapper">
-          <Card>
-            <p>
-              Passionné par l’informatique, j’ai terminé mes études pour devenir
-              ingénieur en bio-informatique.
-            </p>
-            <p>
-              Après 3 ans, à programmer pour les entreprises, j’ai décidé
-              d’élargir mes compétences dans le développement web en passant le
-              titre professionnel.
-            </p>
-            <p>
-              J’ai pu approfondir mes connaissances en HTML, CSS, JavaScript et
-              React avec des projets réalisés seul ou à plusieurs.
-            </p>
-            <p>
-              Par-dessus tout, j’aime apprendre de nouvelles choses et partager
-              mes connaissances.
-            </p>
-          </Card>
-          <Card>
-            <ul className="list-about-me">
-              <li className="list-icon diploma">
-                <h4>Diplômes</h4>
-              </li>
-              <ul>
-                <li>
-                  Titre Professionnel <a href="#">DWWM</a>{' '}
-                  <span>(sept. 2023)</span>
+        <div className="about-section-wrapper">
+          <h3>&Agrave; Propros</h3>
+          <div className="cards-wrapper">
+            <Card>
+              <p>
+                Passionné par l’informatique, j’ai terminé mes études pour
+                devenir ingénieur en bio-informatique.
+              </p>
+              <p>
+                Après 3 ans, à programmer pour les entreprises, j’ai décidé
+                d’élargir mes compétences dans le développement web en passant
+                le titre professionnel.
+              </p>
+              <p>
+                J’ai pu approfondir mes connaissances en HTML, CSS, JavaScript
+                et React avec des projets réalisés seul ou à plusieurs.
+              </p>
+              <p>
+                Par-dessus tout, j’aime apprendre de nouvelles choses et
+                partager mes connaissances.
+              </p>
+            </Card>
+            <Card>
+              <ul className="list-about-me">
+                <li className="list-icon diploma">
+                  <h4>Diplômes</h4>
                 </li>
-                <li>
-                  <a href="#">Master de Bio-Informatique </a>
-                  <span>(avr. 2020)</span>
-                </li>
+                <ul>
+                  <li>
+                    Titre Professionnel{' '}
+                    <a href="https://www.francecompetences.fr/recherche/rncp/31114/">
+                      DWWM
+                    </a>{' '}
+                    <span>(sept. 2023)</span>
+                  </li>
+                  <li>
+                    <a href="#">Master de Bio-Informatique </a>
+                    <span>(avr. 2020)</span>
+                  </li>
+                </ul>
               </ul>
-            </ul>
-            <ul className="list-about-me">
-              <li className="list-icon experience">
-                <h4>Expérience</h4>
-              </li>
-              <ul>
-                <li>5 ans d&apos;expériences dans la programmation</li>
-                <li>
-                  3 ans de travail dans les entreprises de la recherche
-                  publiques
+              <ul className="list-about-me">
+                <li className="list-icon experience">
+                  <h4>Expérience</h4>
                 </li>
+                <ul>
+                  <li>5 ans d&apos;expériences dans la programmation</li>
+                  <li>
+                    3 ans de travail dans les entreprises de la recherche
+                    publiques
+                  </li>
+                </ul>
               </ul>
-            </ul>
-          </Card>
+            </Card>
+          </div>
         </div>
       </section>
     </div>
