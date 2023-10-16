395b408c303e731982c3fbeb4133b12de70c5b57
Ld-monkey <lfourteau91@gmail.com>
2023-10-16 12:48:19 +0200

    test config vite.

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-
-const env = loadEnv('all', process.cwd());
-
 // https://vitejs.dev/config/
-export default defineConfig({
-  
-  plugins: [react()],
-  base: `${env.VITE_URL}`,
-});
+export default ({ mode }) => {
+  // Load app-level env vars to node-level env vars.
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  return defineConfig({
+    // To access env vars here use process.env.TEST_VAR
+    plugins: [react()],
+    base: `${process.env.VITE_URL}`,
+  });
+};
