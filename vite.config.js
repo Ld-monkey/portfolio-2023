b4c3fa236657dbb39c1d73d75a3432edfbe53222
Ld-monkey <lfourteau91@gmail.com>
2023-10-16 12:08:54 +0200

    Add .env file and config vite.

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+
+
+const env = loadEnv('all', process.cwd());
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  
   plugins: [react()],
-  base: '/portfolio-2023/',
+  base: `${env.VITE_URL}`,
 });
